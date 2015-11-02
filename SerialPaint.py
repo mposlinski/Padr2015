fe4c0ee5eb97e9b760f7acb3bfd2ca71433e9081
mposlinski <mposlinski@users.noreply.github.com>
2015-11-02 13:41:11 +0100

    update

--- SerialPaint.py
+++ SerialPaint.py
@@ -29,7 +29,7 @@ print ser.getTimeout()
 
 
 
-timeout = time.time() + 10
+timeout = time.time() + 1
 
 ser.setTimeout(2)
 ser.flushInput()
@@ -37,29 +37,45 @@ im = Image.open("Untitled.bmp")
 print im.format, im.size, im.mode
 # im.show()
 draw = ImageDraw.Draw(im)
-for x in range(10, 630):
-    for y in range(10, 470):
+
+for x in range(0, 640):
+    for y in range(0, 480):
         draw.point((x,y), 0xFFFFFF)
+
+for x in range(0, 5):
+    for y in range(0, 480):
+        draw.point((x,y), 0x0)
+        
+for x in range(635, 640):
+    for y in range(0, 480):
+        draw.point((x,y), 0x0)
+        
+for x in range(0, 640):
+    for y in range(0, 5):
+        draw.point((x,y), 0x0)
+
+for x in range(0, 640):
+    for y in range(475, 480):
+        draw.point((x,y), 0x0)
     
 im.save('Untitled.bmp', "bmp")
 
-
-i = 0
 while True:
     if time.time() > timeout:
-        break
-    receivedLine = ser.readline().split(' ')  # wait for start transmision
-    if receivedLine[0] == "X":
-        print "X = " + receivedLine[2] + "    Y = " + receivedLine[7]
-        x_point = receivedLine[2]
-        y_point = receivedLine[7]
-        x_point = x_point.strip().split('\x00')[0]
+        break   
+    line = ser.readline() # wait for start transmision, read line, remove all spaces
+    receivedLine = line.split('= ')
+    if receivedLine[0] == "X ":
+        x_point = receivedLine[1].strip().split('\x00')[0]
+        y_point = receivedLine[2].strip().split('\x00')[0]
+
         x_ = int(x_point)
-        y_point = y_point.strip().split('\x00')[0]
         y_ = int(y_point)
+        print "X = " + str(x_) + "    Y = " + str(y_)
         y_ = 480 - y_
         draw.point((x_,y_), 0xFF)
         im.save('Untitled.bmp', "bmp")
+        
 del draw
 ser.close() # close port
 print "closed"
